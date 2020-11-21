8edb07f005c117682a8a0e0c769f94f6b17378c7
Maks Zylinski <maksymilian.zylinski@gamivo.com>
2020-11-21 22:08:26 +0100

    sass setup

---
+++ src/Pokedex/Pokedex.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Pokedex.scss';
+
+const Pokedex = () => {
+    return (
+        <div className="Pokedex">
+            <p>no elo świry</p>
+        </div>
+    )
+}
+
+export default Pokedex;
